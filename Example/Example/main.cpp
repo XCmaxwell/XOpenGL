a94011eba26ae108b845d321479a836993ce41a2
xcwang24 <xcwang24@iflytek.com>
2023-12-02 20:07:08 +0800

    point

--- Example/Example/main.cpp
+++ Example/Example/main.cpp
@@ -16,41 +16,113 @@
 
 using namespace std;
 
+// 3D模型--> 图元--> 顶点
+// 顶点着色器/片段着色器 --> 管线
+
+
+/*
+/// 通知 OpenGL 构建三角形-->管线中的GLSL 代码开始执行
+/// - Parameter mode: 图元类型（三角形 GL_TRIANGLES ）
+/// - Parameter first: 从哪个顶点开始绘制
+/// - Parameter first: 总共要绘制的顶点数
+glDrawArrays(GLenum mode, Glint first, GLsizei count);
+ */
+
+#define numVAOs 1
+GLuint renderingProgram;
+GLuint vao[numVAOs];
+
+GLuint createShaderProgram() {
+    
+    const char *vshaderSource =
+        "#version 410    \n"
+        "void main(void) \n"
+        "{ gl_Position = vec4(0.0, 0.0, 0.0, 1.0); }";
+
+    const char *fshaderSource =
+        "#version 410    \n"
+        "out vec4 color; \n"
+        "void main(void) \n"
+        "{ color = vec4(0.0, 0.0, 1.0, 1.0); }";
+    
+    //顶点
+    GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
+    //
+    GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(vShader, 1, &vshaderSource, NULL);
+    glShaderSource(fShader, 1, &fshaderSource, NULL);
+    glCompileShader(vShader);
+    glCompileShader(fShader);
+    
+    GLuint vfProgram = glCreateProgram();
+    glAttachShader(vfProgram, vShader);
+    glAttachShader(vfProgram, fShader);
+    glLinkProgram(vfProgram);
+    return vfProgram;
+}
+
 void init(GLFWwindow* window) {
     
+    // 储存指向它的整数 ID
+    renderingProgram = createShaderProgram();
+    glGenVertexArrays(numVAOs, vao);
+    glBindVertexArray(vao[0]);
 }
 
 void display(GLFWwindow* window, double currentTime) {
-    glClearColor(1.0, 1.0, 0.0, 1.0);
+    
+    //指定了清除背景时用的颜色值
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    //清除全部缓冲区---使用clear Color填充缓冲区
     glClear(GL_COLOR_BUFFER_BIT);
+    
+    glUseProgram(renderingProgram);
+    glPointSize(30.0f);
+    glDrawArrays(GL_POINTS, 0, 1);
 }
 
 int main(int argc, const char * argv[]) {
     
-    if (!glfwInit()) { exit(EXIT_FAILURE); }
+    //1.初始化glfw库
+    if (!glfwInit()) {
+        exit(EXIT_FAILURE);
+    }
+    //指定了计算机必须与 OpenGL版本4.1兼容
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
-    
+    //Mac专有
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     
+    //初始化window
+    //创建与其相关的OpenGL上下文、前2个参数指定宽高标题等
+    //后2个参数 NULL， 分别用来允许全屏显示和资源共享
     GLFWwindow * window = glfwCreateWindow(600, 600, "Chapter 2 - program 1", NULL, NULL);
+    //将window与 当前 OpenGL 上下文关联
     glfwMakeContextCurrent(window);
     glViewport(0,0,600,600);
     glewExperimental = GL_TRUE;
+    
+    //2.初始化glew库
     if (glewInit() != GLEW_OK) {
         exit(EXIT_FAILURE);
     }
+    //开启垂直同步
     glfwSwapInterval(1);
 
+    //3.初始化函数
     init(window);
 
+    //4.重复渲染
     while (!glfwWindowShouldClose(window)) {
         display(window, glfwGetTime());
+        //绘制屏幕
         glfwSwapBuffers(window);
+        //处理窗口相关事件
         glfwPollEvents();
     }
 
+    //销毁窗口、终止运行
     glfwDestroyWindow(window);
     glfwTerminate();
     exit(EXIT_SUCCESS);

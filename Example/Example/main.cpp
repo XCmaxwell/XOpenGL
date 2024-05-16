c58e931e103e52c3df769c809c66e7f146e223d4
xcwang24 <xcwang24@iflytek.com>
2024-05-16 15:31:48 +0800

    2.6 C++ file  struct

--- Example/Example/main.cpp
+++ Example/Example/main.cpp
@@ -14,7 +14,8 @@
 #define GLEW_STATIC
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-#include "CHeckErrorHeader.h"
+
+#include "Utils.h"
 
 using namespace std;
 
@@ -35,75 +36,18 @@ glDrawArrays(GLenum mode, Glint first, GLsizei count);
 #define numVAOs 1
 GLuint _renderingProgram;
 GLuint _vao[numVAOs];
-
-string readShaderSource(const char *filePath) {
-    string content;
-    ifstream file_stream(filePath, ios::in);
-    if ( !file_stream.is_open() ) {
-        return "";
-    }
-    string line = "";
-    while (!file_stream.eof()) {
-        getline(file_stream, line);
-        content.append(line + "\n");
-    }
-    file_stream.close();
-    return content;
-}
-
-GLuint createShaderProgram() {
-    
-    string vshaderString = readShaderSource("/Users/admin/Study/XOpenGL/Example/Example/vertShader.glsl");
-    if (vshaderString.length() == 0) {
-        return 0;
-    }
-    const char *vshaderSource = vshaderString.c_str();
-    
-    string fshaderString = readShaderSource("/Users/admin/Study/XOpenGL/Example/Example/fragShader.glsl");
-    if (fshaderString.length() == 0) {
-        return 0;
-    }
-    const char *fshaderSource = fshaderString.c_str();
-
-    //创建 顶点着色器
-    GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
-    //创建 片段着色器
-    GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
-    //加载着色器资源
-    glShaderSource(vShader, 1, &vshaderSource, NULL);
-    glShaderSource(fShader, 1, &fshaderSource, NULL);
-    //编译着色器
-    glCompileShader(vShader);
-    checkOpenGLError();
-    
-    glCompileShader(fShader);
-    checkOpenGLError();
-    
-    //创建一个空的程序对象，并返回一个可以引用它的非零值。
-    //1.将来用于链接着色器对象， 2.检查着色器兼容性
-    GLuint vfProgram = glCreateProgram();
-    //着色器链接到程序对象, 相当于绑定
-    glAttachShader(vfProgram, vShader);
-    glAttachShader(vfProgram, fShader);
-    
-    //连接程序对象
-    glLinkProgram(vfProgram);
-    checkOpenGLError();
-    return vfProgram;
-}
+static float x = 0.0f; // 三角形在 x 轴的位置
+static float moveX = 0.01f; // 移动三角形的偏移量
 
 void init(GLFWwindow* window) {
     
     // 储存指向它的整数 ID
-    _renderingProgram = createShaderProgram();
+    _renderingProgram = Utils::createShaderProgram("/Users/admin/Study/XOpenGL/Example/Example/vertShader.glsl", "/Users/admin/Study/XOpenGL/Example/Example/fragShader.glsl");
     // 创建一个或多个顶点数组对象的标识符
     glGenVertexArrays(numVAOs, _vao);
     // 将创建的顶点数组对象绑定到当前OpenGL上下文
     glBindVertexArray(_vao[0]);
 }
-
-static float x = 0.0f; // 三角形在 x 轴的位置
-static float moveX = 0.01f; // 移动三角形的偏移量
 
 void display(GLFWwindow* window, double currentTime) {
     
@@ -124,7 +68,7 @@ void display(GLFWwindow* window, double currentTime) {
     //绑定新的着色器程序
     glUseProgram(_renderingProgram);
     //glPointSize用于指定一个顶点对应渲染的像素点数，glPointSize来设置点的直径大小
-    glPointSize(30.0f);
+    glPointSize(1.0f);
     //从已经绑定的顶点数组对象（VAO）中提取顶点数据
     //0.要渲染的图元类型:点、线、三角形， 1.开始渲染的第一个顶点的索引  2.渲染的顶点数量。
     glDrawArrays(GL_TRIANGLES, 0, 3);
@@ -146,12 +90,12 @@ int main(int argc, const char * argv[]) {
     
     //初始化window
     //创建与其相关的OpenGL上下文、前2个参数指定宽高标题等
-    //后2个参数 NULL， 分别用来允许全屏显示和资源共享
-    GLFWwindow * window = glfwCreateWindow(300, 300, "Chapter 2 - program 1", NULL, NULL);
+    //后2个参数 NULL, 分别用来允许全屏显示和资源共享
+    GLFWwindow * window = glfwCreateWindow(600, 600, "Chapter 2 - program 1", NULL, NULL);
     //将window 与 当前 OpenGL 上下文关联
     glfwMakeContextCurrent(window);
     //选择绘制区域
-    glViewport(0,0,300,300);
+    glViewport(0,0,600,600);
     //启用GLEW库的实验性功能
     //有些显卡的驱动程序不能正确给出所支持的扩展信息，导致GLEW不能正确获取某些函数的入口地址
     glewExperimental = GL_TRUE;
